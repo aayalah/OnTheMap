eb4238ce06caf8ca6edd6310d802fb9d1d020c34
Alejandro Ayala-Hurtado <amayala7@yahoo.com>
2017-01-08 21:04:50 -0800

    Copied in Facebook Frameworks

--- OnTheMap/ParseConvenience.swift
+++ OnTheMap/ParseConvenience.swift
@@ -13,12 +13,12 @@ extension ParseClient {
     func authenticateViaUdacity(userName: String, passWord: String, completionHandlerForAuth: @escaping (Bool, NSError?) -> Void){
         
         let parameters = [String: AnyObject]();
-        let jsonBody = "{\"\(ParseClient.AuthenticationPostParameters.bodyKey)\": {\"\(ParseClient.AuthenticationPostParameters.username)\": \"\(userName)\",\"\(ParseClient.AuthenticationPostParameters.password)\": \"\(passWord)\"}}"
-        _ = taskForPostAuthenticationMethod(ParseClient.Constants.Auth_URL, parameters: parameters, jsonBody: jsonBody) { result, error in
+        let jsonBody = "{\"\(AuthenticationPostParameters.bodyKey)\": {\"\(AuthenticationPostParameters.username)\": \"\(userName)\",\"\(AuthenticationPostParameters.password)\": \"\(passWord)\"}}"
+        _ = taskForPostAuthenticationMethod(Constants.Auth_URL, parameters: parameters, jsonBody: jsonBody) { result, error in
             if error == nil {
                 let result = result as! [String: AnyObject]
-                let accountObject = result[ParseClient.JSONResponseKeys.account] as! [String: AnyObject]
-                self.userModel.getCurrentUser().setKey(val: (accountObject[ParseClient.JSONResponseKeys.key] as? String)!)
+                let accountObject = result[JSONResponseKeys.account] as! [String: AnyObject]
+                self.userModel.getCurrentUser().setKey(val: (accountObject[JSONResponseKeys.key] as? String)!)
                 completionHandlerForAuth(true,nil)
             } else {
                 completionHandlerForAuth(false, error!)
@@ -33,13 +33,13 @@ extension ParseClient {
     func authenticateViaFacebook(token: String, completionHandlerForAuth: @escaping (Bool, NSError?) -> Void) {
         
         let parameters = [String: AnyObject]();
-        let jsonBody = "{\"\(ParseClient.FBAuthenticationPostParameters.bodyKey)\": {\"\(ParseClient.FBAuthenticationPostParameters.accessToken)\": \"\(token)\"}}"
+        let jsonBody = "{\"\(FBAuthenticationPostParameters.bodyKey)\": {\"\(FBAuthenticationPostParameters.accessToken)\": \"\(token)\"}}"
         
-        _ = taskForPostAuthenticationMethod(ParseClient.Constants.Auth_URL, parameters: parameters, jsonBody: jsonBody) { result, error in
+        _ = taskForPostAuthenticationMethod(Constants.Auth_URL, parameters: parameters, jsonBody: jsonBody) { result, error in
             if error == nil {
                 let result = result as! [String: AnyObject]
-                let accountObject = result[ParseClient.JSONResponseKeys.account] as! [String: AnyObject]
-                self.userModel.getCurrentUser().setKey(val: (accountObject[ParseClient.JSONResponseKeys.key] as? String)!)
+                let accountObject = result[JSONResponseKeys.account] as! [String: AnyObject]
+                self.userModel.getCurrentUser().setKey(val: (accountObject[JSONResponseKeys.key] as? String)!)
                 completionHandlerForAuth(true,nil)
             } else {
                 completionHandlerForAuth(false, error!)
@@ -53,10 +53,10 @@ extension ParseClient {
     
     func getStudentsLocations(limit: Int, skip: Int, storeResult: @escaping (_ result: AnyObject?) -> Bool, completionHandler: @escaping (Bool, NSError?) -> Void) {
        
-        var parameters = [ParseClient.ClassesGetParameters.limit : "\(limit)"]
-        parameters[ParseClient.ClassesGetParameters.skip] = "\(skip)"
-        parameters[ParseClient.ClassesGetParameters.recent] = "-\(ParseClient.ClassesGetParameters.updatedOrder)"
-        _ = taskForGetMethod(ParseClient.ClassesMethods.Locations, parameters: parameters as [String : AnyObject], jsonBody: "") { result, error in
+        var parameters = [ClassesGetParameters.limit : "\(limit)"]
+        parameters[ClassesGetParameters.skip] = "\(skip)"
+        parameters[ClassesGetParameters.recent] = "-\(ClassesGetParameters.updatedOrder)"
+        _ = taskForGetMethod(ClassesMethods.Locations, parameters: parameters as [String : AnyObject], jsonBody: "") { result, error in
             if error == nil {
                 
                 completionHandler(storeResult(result), nil)
@@ -73,9 +73,9 @@ extension ParseClient {
     
     func getStudentLocation(uniqueKey: String, storeResult: @escaping (_ result: AnyObject?) -> Bool, completionHandler: @escaping (Bool, NSError?) -> Void) {
         var parameters:[String:AnyObject] = [String:AnyObject]()
-        parameters[ParseClient.ClassesGetParameters.where_param] = "{\"\(ParseClient.ClassesGetParameters.uniqueKey)\":\"\(uniqueKey)\"}" as AnyObject?
+        parameters[ClassesGetParameters.where_param] = "{\"\(ClassesGetParameters.uniqueKey)\":\"\(uniqueKey)\"}" as AnyObject?
         
-        _ = taskForGetMethod(ParseClient.ClassesMethods.Locations, parameters: parameters as [String : AnyObject], jsonBody: "") { result, error in
+        _ = taskForGetMethod(ClassesMethods.Locations, parameters: parameters as [String : AnyObject], jsonBody: "") { result, error in
             if error == nil {
 
                 completionHandler(storeResult(result), nil)
@@ -91,9 +91,9 @@ extension ParseClient {
     
     func postStudentLocation(uniqueKey: String, firstName: String, lastName: String, mapString: String, mediaURL: String, latitude: Float, longitude: Float, completionHandler: @escaping (Bool, NSError?) -> Void) {
         let parameters:[String:AnyObject] = [String:AnyObject]()
-        let jsonBody = "{\"\(ParseClient.ClassesPostParameters.uniqueKey)\": \"\(uniqueKey)\", \"\(ParseClient.ClassesPostParameters.firstName)\": \"\(firstName)\", \"\(ParseClient.ClassesPostParameters.lastName)\": \"\(lastName)\",\"\(ParseClient.ClassesPostParameters.mapString)\": \"\(mapString)\", \"\(ParseClient.ClassesPostParameters.mediaURL)\": \"\(mediaURL)\",\"\(ParseClient.ClassesPostParameters.latitude)\": \(latitude), \"\(ParseClient.ClassesPostParameters.longitude)\": \(longitude)}"
+        let jsonBody = "{\"\(ClassesPostParameters.uniqueKey)\": \"\(uniqueKey)\", \"\(ClassesPostParameters.firstName)\": \"\(firstName)\", \"\(ClassesPostParameters.lastName)\": \"\(lastName)\",\"\(ClassesPostParameters.mapString)\": \"\(mapString)\", \"\(ClassesPostParameters.mediaURL)\": \"\(mediaURL)\",\"\(ClassesPostParameters.latitude)\": \(latitude), \"\(ClassesPostParameters.longitude)\": \(longitude)}"
 
-        _ = taskForPostMethod(ParseClient.ClassesMethods.Locations, parameters: parameters as [String : AnyObject], jsonBody: jsonBody) { result, error in
+        _ = taskForPostMethod(ClassesMethods.Locations, parameters: parameters as [String : AnyObject], jsonBody: jsonBody) { result, error in
             if error == nil {
                 
                 completionHandler(true, nil)
@@ -110,10 +110,10 @@ extension ParseClient {
     //need to fix
     func putStudentLocation(uniqueKey: String, firstName: String, lastName: String, mapString: String, mediaURL: String, latitude: Float, longitude: Float, objectId: String, completionHandler: @escaping (Bool, NSError?) -> Void) {
         let parameters:[String:AnyObject] = [String:AnyObject]()
-        let jsonBody = "{\"\(ParseClient.ClassesPostParameters.uniqueKey)\": \"\(uniqueKey)\", \"\(ParseClient.ClassesPostParameters.firstName)\": \"\(firstName)\", \"\(ParseClient.ClassesPostParameters.lastName)\": \"\(lastName)\",\"\(ParseClient.ClassesPostParameters.mapString)\": \"\(mapString)\", \"\(ParseClient.ClassesPostParameters.mediaURL)\": \"\(mediaURL)\",\"\(ParseClient.ClassesPostParameters.latitude)\": \(latitude), \"\(ParseClient.ClassesPostParameters.longitude)\": \(longitude)}"
+        let jsonBody = "{\"\(ClassesPostParameters.uniqueKey)\": \"\(uniqueKey)\", \"\(ClassesPostParameters.firstName)\": \"\(firstName)\", \"\(ClassesPostParameters.lastName)\": \"\(lastName)\",\"\(ClassesPostParameters.mapString)\": \"\(mapString)\", \"\(ClassesPostParameters.mediaURL)\": \"\(mediaURL)\",\"\(ClassesPostParameters.latitude)\": \(latitude), \"\(ClassesPostParameters.longitude)\": \(longitude)}"
         
         
-        _ = taskForPutMethod(addParamToMethod(value: objectId, method: ParseClient.ClassesMethods.LocationsUserUpdate, key: ParseClient.URLKeys.objectId), parameters: parameters, jsonBody: jsonBody) { result, error in
+        _ = taskForPutMethod(addParamToMethod(value: objectId, method: ClassesMethods.LocationsUserUpdate, key: URLKeys.objectId), parameters: parameters, jsonBody: jsonBody) { result, error in
             if error == nil {
                 
                 
@@ -132,9 +132,9 @@ extension ParseClient {
     func getStudentInfo(uniqueKey: String, storeResult: @escaping (_ result: AnyObject?) -> Bool, completionHandler: @escaping (Bool, NSError?) -> Void) {
         
         
-        let url = ParseClient.Constants.UserData
+        let url = Constants.UserData
         let parameters:[String:AnyObject] = [String:AnyObject]()
-        _ = taskForGetUserInfoMethod(addParamToMethod(value: uniqueKey, method: url, key: ParseClient.URLKeys.objectId), parameters: parameters, jsonBody: "") { result, error in
+        _ = taskForGetUserInfoMethod(addParamToMethod(value: uniqueKey, method: url, key: URLKeys.objectId), parameters: parameters, jsonBody: "") { result, error in
             
             if error == nil {
                 
@@ -158,7 +158,7 @@ extension ParseClient {
         
         let parameters = [String:AnyObject]()
 
-        _ = taskForDeleteAuthMethod(ParseClient.Constants.Auth_URL, parameters: parameters as [String : AnyObject]) { result, error in
+        _ = taskForDeleteAuthMethod(Constants.Auth_URL, parameters: parameters as [String : AnyObject]) { result, error in
             if error == nil {
                 
                 completionHandler(true, nil)
